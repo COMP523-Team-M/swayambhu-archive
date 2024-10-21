ed3c1e5c93becabe5bca438a400857a1857b4089
Evan Murray <evanesce@email.unc.edu>
2024-10-20 20:52:46 -0400

    Update layout, and search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <div className="my-5 flex flex-col items-center">
+    <div className="my-10">
       <SearchBar />
     </div>
   );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,15 +3,15 @@ import { FiSearch } from "react-icons/fi";
 export default function SearchBar() {
   return (
     <>
-      <form>
-        <div className="relative flex items-center text-gray-400 focus-within:text-gray-600">
+      <form className="mx-auto w-4/5">
+        <div className="flex items-center text-gray-400 focus-within:text-gray-600">
           <FiSearch className="pointer-events-none absolute ml-3 h-5 w-5 transition-all duration-300" />
           <input
             type="text"
             name="search"
             placeholder="Search..."
             autoComplete="off"
-            className="focus: rounded-3xl px-3 py-2 pl-10 pr-3 shadow-md outline-none ring-[0.5px] ring-gray-400 transition-all duration-300 focus:ring-2 focus:ring-blue-200"
+            className="w-full rounded-3xl px-3 py-2 pl-10 pr-3 shadow-md outline-none ring-[0.5px] ring-gray-400 transition-all duration-300 focus:ring-2 focus:ring-blue-200"
           />
         </div>
       </form>
