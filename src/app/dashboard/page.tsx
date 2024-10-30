51f28cb9acbeac9f508414b52ffa81b477892856
Evan Murray <evanesce@email.unc.edu>
2024-10-30 14:50:19 -0400

    Add authentication

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function page() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function page() {
+  const { isAuthenticated } = getKindeServerSession();
+  const isLoggedIn = await isAuthenticated();
+  if (!isLoggedIn) {
+    redirect("/api/auth/login");
+  }
+
   return <div>dashboard works!</div>;
 }
