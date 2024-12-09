fd9594be55179cb14870dc622cf385f77c9fcd33
Evan Murray <evanesce@email.unc.edu>
2024-12-09 16:22:21 -0500

    Make get-all route dynamic (fix deployment)

--- src/app/api/elasticsearch/CRUD/get-all-videos/route.ts
+++ src/app/api/elasticsearch/CRUD/get-all-videos/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import client from "@/utils-ts/elasticsearch";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const response = await client.search({
